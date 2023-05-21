708f3a14020c1ff17d79082c5f0f95302d88d315
Ritik Kumar (Ravencrest) <ritiksingh1859@gmail.com>
2023-05-21 19:45:32 +0530

    updates 2.2

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
-// First loader
-
 window.addEventListener('load', function() {
   var center = document.querySelector('.center');
   var container = document.querySelector('.container');
   var button = document.querySelector('.button');
   var hexCodes = ['#474747', '#C7CEDB', '#f7ce5b', '#E7DECC'];
   var wordHex = ['#E7DECC', '#EC0B43', '#5DA271', '#474747'];
-  var fadeDuration = 500; // 1.5 seconds
-  var minDuration = 10500; // Minimum duration in milliseconds
-
-  // Calculate the number of iterations required to meet the minimum duration
-  var iterations = Math.ceil(minDuration / (fadeDuration * hexCodes.length));
+  var fadeDuration = 500; // 0.5 seconds
 
   // Function to change the background color and fade away
   function changeBackgroundColorAndFade() {
     var index = 0;
-    var iterationCount = 0;
     var colorInterval = setInterval(function() {
       center.style.backgroundColor = hexCodes[index % hexCodes.length];
       center.style.color = wordHex[index % wordHex.length];
       index++;
       if (index >= hexCodes.length) {
-        iterationCount++;
-        if (iterationCount >= iterations) {
-          clearInterval(colorInterval);
-          center.classList.add('hidden');
-          container.classList.remove('hidden');
-          button.classList.remove('hidden');
-        }
+        clearInterval(colorInterval);
+        center.classList.add('hidden');
+        container.classList.remove('hidden');
+        button.classList.remove('hidden');
       }
     }, fadeDuration);
   }
@@ -35,19 +25,12 @@ window.addEventListener('load', function() {
   // Start the background color change and fade after the page loads
   setTimeout(changeBackgroundColorAndFade, 0); // Start immediately
 
-  // Hide the center element after 8 seconds
-  setTimeout(function() {
-    center.classList.add('hidden');
-    container.classList.remove('hidden');
-    button.classList.remove('hidden');
-  }, 8000);
-});
+  // Add event listener to the button for hiding the container
+  button.addEventListener('click', function() {
+    container.classList.add('hidden');
+  });
 
-
-// Button
-window.addEventListener('load', function() {
-  var button = document.querySelector('.button');
-
+  // Button
   function setButtonDimensions() {
     var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
     var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
