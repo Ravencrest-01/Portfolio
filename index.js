ab68420c214902325ded9d5dff7beab1d2d5114a
Ritik Kumar (Ravencrest) <ritiksingh1859@gmail.com>
2023-06-09 19:50:36 +0530

    Updates 2.3

--- index.js
+++ index.js
@@ -21,6 +21,28 @@ const skillsNav = document.querySelector('.skills');
 const homeContents = document.querySelector('.homeContent');
 const aboutContents = document.querySelector('.aboutContent');
 const skillsContents = document.querySelector('.skillsContent');
+const exploreContainer = document.querySelector('.explorerContainer')
+
+const uiDropper = document.querySelector('.uiDrop');
+const uiArrowDown = document.querySelector('.uiArrow');
+
+const threeDropper = document.querySelector('.threeDrop');
+const threeArrowDown = document.querySelector('.threeArrow');
+
+const adobeDropper = document.querySelector('.adobeDrop');
+const adobeArrowDown = document.querySelector('.adobeArrow');
+
+const languageDropper = document.querySelector('.languagesDrop');
+const languageArrowDown = document.querySelector('.languageArrow');
+
+const dsaDropper = document.querySelector('.dsaDrop');
+const dsaArrowDown = document.querySelector('.dsaArrow');
+
+const webDropper = document.querySelector('.htmlDrop');
+const webArrowDown = document.querySelector('.htmlArrow');
+
+const freeDropper = document.querySelector('.freeDrop');
+const freeArrowDown = document.querySelector('.freeArrow');
 
 // Navbar EventListners
 homeNav.addEventListener('click',()=>{
@@ -28,6 +50,8 @@ homeNav.addEventListener('click',()=>{
         if(!aboutNav.classList.contains('focused')){
             skillsNav.classList.remove('focused');
             skillsContents.classList.add('hidden');
+            aboutContents.classList.add('hidden');
+            exploreContainer.classList.add('hidden');
 
             homeNav.classList.add('focused');
             homeContents.classList.remove('hidden');
@@ -35,6 +59,8 @@ homeNav.addEventListener('click',()=>{
 
         aboutNav.classList.remove('focused');
         aboutContents.classList.add('hidden');
+        skillsContents.classList.add('hidden');
+        exploreContainer.classList.add('hidden');
 
         homeNav.classList.add('focused');
         homeContents.classList.remove('hidden');
@@ -45,23 +71,29 @@ aboutNav.addEventListener('click',()=>{
         if(!homeNav.classList.contains('focused')){
             skillsNav.classList.remove('focused');
             skillsContents.classList.add('hidden');
+            homeContents.classList.add('hidden');
 
             aboutNav.classList.add('focused');
             aboutContents.classList.remove('hidden');
+            exploreContainer.classList.remove('hidden');
         }
 
         homeNav.classList.remove('focused');
         homeContents.classList.add('hidden');
+        skillsContents.classList.add('hidden');
 
         aboutNav.classList.add('focused');
         aboutContents.classList.remove('hidden');
+        exploreContainer.classList.remove('hidden');
     }
 });
 skillsNav.addEventListener('click',()=>{
     if(!skillsNav.classList.contains('focused')) {
         if(!aboutNav.classList.contains('focused')){
-            homesNav.classList.remove('focused');
-            homesContents.classList.add('hidden');
+            homeNav.classList.remove('focused');
+            homeContents.classList.add('hidden');
+            aboutContents.classList.add('hidden');
+            exploreContainer.classList.add('hidden');
 
             skillsNav.classList.add('focused');
             skillsContents.classList.remove('hidden');
@@ -69,12 +101,13 @@ skillsNav.addEventListener('click',()=>{
 
         aboutNav.classList.remove('focused');
         aboutContents.classList.add('hidden');
+        homeContents.classList.add('hidden');
+        exploreContainer.classList.add('hidden');
 
         skillsNav.classList.add('focused');
         skillsContents.classList.remove('hidden');
     }
 });
-
 
 smallNavItems.forEach(item => {
     item.addEventListener('click', () =>{
@@ -86,6 +119,7 @@ smallNavItems.forEach(item => {
 });
 
 
+// Link opener Footer
 twitterDiv.addEventListener('mouseenter', () => {
     twitterIcon.style.transition = 'color 0.3s ease';
     twitterIcon.style.color='#ffffff';
@@ -150,13 +184,111 @@ container1.addEventListener('click', () => {
   
 container2.addEventListener('click', () => {
     if (!container2.classList.contains('projectFocused')) {
-    //   if (container1.classList.contains('projectFocused')) {
         container1.classList.remove('projectFocused');
         container1.classList.add('projectUnfocused');
-    //   }
 
       container2.classList.remove('projectUnfocused');
       container2.classList.add('projectFocused');
       container2.style.transition = 'transform 0.7s ease, filter 0.7s ease, opacity 0.7s ease';
+    }
+});
+
+
+uiDropper.addEventListener('click', ()=>{
+    if(!uiDropper.classList.contains('dropped')) {
+        uiDropper.classList.add('dropped');
+        uiArrowDown.classList.remove('fa-chevron-right');
+        uiArrowDown.classList.add('fa-chevron-down');
+    }
+
+    else{
+        uiDropper.classList.remove('dropped');
+        uiArrowDown.classList.remove('fa-chevron-down');
+        uiArrowDown.classList.add('fa-chevron-right');
+    }
+});
+
+threeDropper.addEventListener('click', ()=>{
+    if(!threeDropper.classList.contains('dropped')) {
+        threeDropper.classList.add('dropped');
+        threeArrowDown.classList.remove('fa-chevron-right');
+        threeArrowDown.classList.add('fa-chevron-down');
+    }
+
+    else{
+        threeDropper.classList.remove('dropped');
+        threeArrowDown.classList.remove('fa-chevron-down');
+        threeArrowDown.classList.add('fa-chevron-right');
+    }
+});
+
+adobeDropper.addEventListener('click', ()=>{
+    if(!adobeDropper.classList.contains('dropped')) {
+        adobeDropper.classList.add('dropped');
+        adobeArrowDown.classList.remove('fa-chevron-right');
+        adobeArrowDown.classList.add('fa-chevron-down');
+    }
+
+    else{
+        adobeDropper.classList.remove('dropped');
+        adobeArrowDown.classList.remove('fa-chevron-down');
+        adobeArrowDown.classList.add('fa-chevron-right');
+    }
+});
+
+
+languageDropper.addEventListener('click', ()=>{
+    if(!languageDropper.classList.contains('dropped')) {
+        languageDropper.classList.add('dropped');
+        languageArrowDown.classList.remove('fa-chevron-right');
+        languageArrowDown.classList.add('fa-chevron-down');
+    }
+
+    else{
+        languageDropper.classList.remove('dropped');
+        languageArrowDown.classList.remove('fa-chevron-down');
+        languageArrowDown.classList.add('fa-chevron-right');
+    }
+});
+
+dsaDropper.addEventListener('click', ()=>{
+    if(!dsaDropper.classList.contains('dropped')) {
+        dsaDropper.classList.add('dropped');
+        dsaArrowDown.classList.remove('fa-chevron-right');
+        dsaArrowDown.classList.add('fa-chevron-down');
+    }
+
+    else{
+        dsaDropper.classList.remove('dropped');
+        dsaArrowDown.classList.remove('fa-chevron-down');
+        dsaArrowDown.classList.add('fa-chevron-right');
+    }
+});
+
+webDropper.addEventListener('click', ()=>{
+    if(!webDropper.classList.contains('dropped')) {
+        webDropper.classList.add('dropped');
+        webArrowDown.classList.remove('fa-chevron-right');
+        webArrowDown.classList.add('fa-chevron-down');
+    }
+
+    else{
+        webDropper.classList.remove('dropped');
+        webArrowDown.classList.remove('fa-chevron-down');
+        webArrowDown.classList.add('fa-chevron-right');
+    }
+});
+
+freeDropper.addEventListener('click', ()=>{
+    if(!freeDropper.classList.contains('dropped')) {
+        freeDropper.classList.add('dropped');
+        freeArrowDown.classList.remove('fa-chevron-right');
+        freeArrowDown.classList.add('fa-chevron-down');
+    }
+
+    else{
+        freeDropper.classList.remove('dropped');
+        freeArrowDown.classList.remove('fa-chevron-down');
+        freeArrowDown.classList.add('fa-chevron-right');
     }
 });
