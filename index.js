a430a112e8990595782ee01a3a3492f38bb2e1db
Ritik Kumar (Ravencrest) <ritiksingh1859@gmail.com>
2023-06-09 12:25:43 +0530

    Update 2.1

--- index.js
+++ index.js
@@ -59,8 +59,7 @@ function updateTime() {
   const currentTime = new Date();
   const hours = currentTime.getHours().toString().padStart(2, '0');
   const minutes = currentTime.getMinutes().toString().padStart(2, '0');
-  const seconds = currentTime.getSeconds().toString().padStart(2, '0');
-  const timeString = `${hours}:${minutes}:${seconds}`;
+  const timeString = `${hours}:${minutes}`;
   timeElement.textContent = timeString;
 }
 
