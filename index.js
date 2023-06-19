5aa3f2e6c3e923b22d0a611de1eafbe86dda702b
Ritik Kumar (Ravencrest) <ritiksingh1859@gmail.com>
2023-06-19 17:50:18 +0530

    minor updates

--- index.js
+++ index.js
@@ -58,9 +58,15 @@ const glowChanger = document.querySelector('.glower');
 const qrHidder = document.querySelector('.yellow');
 const rickQR = document.querySelector('.rickQR');
 
-// const gmailOpner = document.querySelector('.green');
-// const contactOpener = document.querySelector('.contact');
-// const canvas = document.getElementById('canvasScratcher');
+const gmailOpner = document.querySelector('.email');
+const emailIcon = document.querySelector('.fa-envelope');
+
+const contactOpener = document.querySelector('.contact');
+const canvas = document.getElementById('canvasScratcher');
+const ctx = canvas.getContext('2d');
+
+
+
 
 qrHidder.addEventListener('click', ()=>{
     if(!qrHidder.classList.contains('clicked')){
@@ -71,22 +77,7 @@ qrHidder.addEventListener('click', ()=>{
         qrHidder.classList.remove('clicked');
         rickQR.classList.add('hidden');
     }
-})
-
-
-// gmailOpner.addEventListener('click', executeAfterClick => {
-//     console.log('hi');
-//     if (!gmailOpner.classList.contains('clicked')) {
-//       gmailOpner.classList.add('clicked');
-//       contactOpener.classList.remove('hidden');
-//       canvas.style.display = 'block';
-//     } else {
-//       gmailOpner.classList.remove('clicked');
-//       contactOpener.classList.add('hidden');
-//       canvas.style.display = 'none';
-//     }
-//   });
-
+});
 
 
 // colorchanger full
@@ -273,6 +264,15 @@ projectsNav.addEventListener('click',()=>{
 
 
 // Link opener Footer
+gmailOpner.addEventListener('mouseenter', () => {
+    emailIcon.style.transition = 'color 0.3s ease';
+    emailIcon.style.color='#ffffff';
+});
+gmailOpner.addEventListener('mouseleave', () => {
+    emailIcon.style.transition = 'color 0.3s ease';
+    emailIcon.style.color='rgb(144, 57, 168)';
+});
+
 twitterDiv.addEventListener('mouseenter', () => {
     twitterIcon.style.transition = 'color 0.3s ease';
     twitterIcon.style.color='#ffffff';
@@ -485,126 +485,34 @@ function handleKeyPress(event) {
   }
 
 
-  // Get the canvas element
-
-//   function executeAfterClick() {
-//     const ctx = canvas.getContext('2d');
-  
-//     // Set the canvas size
-//     canvas.width = canvas.clientWidth;
-//     canvas.height = canvas.clientHeight;
-  
-//     // Set initial state
-//     let isDrawing = false;
-//     let lastX = 0;
-//     let lastY = 0;
-//     let revealThreshold = 0.7; // Percentage of scratched area to trigger reveal
-  
-//     // Initialize the scratcher
-//     initCanvas();
-  
-//     // Event listeners for drawing
-//     canvas.addEventListener('mousedown', startDrawing);
-//     canvas.addEventListener('mousemove', draw);
-//     canvas.addEventListener('mouseup', stopDrawing);
-//     canvas.addEventListener('mouseout', stopDrawing);
-  
-//     function initCanvas() {
-//       // Fill the canvas with a grey color
-//       ctx.fillStyle = '#808080';
-//       ctx.fillRect(0, 0, canvas.width, canvas.height);
-//     }
-  
-//     function startDrawing(e) {
-//       isDrawing = true;
-//       [lastX, lastY] = [e.offsetX, e.offsetY];
-//     }
-  
-//     function draw(e) {
-//       if (!isDrawing) return;
-  
-//       ctx.globalCompositeOperation = 'destination-out'; // Reveal the content
-//       ctx.beginPath();
-//       ctx.moveTo(lastX, lastY);
-//       ctx.lineTo(e.offsetX, e.offsetY);
-//       ctx.lineWidth = 20;
-//       ctx.lineCap = 'round';
-//       ctx.strokeStyle = 'rgba(0, 0, 0, 1)';
-//       ctx.stroke();
-  
-//       [lastX, lastY] = [e.offsetX, e.offsetY];
-  
-//       // Check the percentage of the canvas revealed
-//       const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-//       const pixels = imageData.data;
-//       let totalTransparentPixels = 0;
-  
-//       for (let i = 0; i < pixels.length; i += 4) {
-//         if (pixels[i + 3] === 0) {
-//           totalTransparentPixels++;
-//         }
-//       }
-  
-//       const revealedAreaPercentage = totalTransparentPixels / (pixels.length / 4);
-  
-//       // If the revealed area exceeds the threshold, reveal the email beneath
-//       if (revealedAreaPercentage >= revealThreshold) {
-//         revealEmail();
-//       }
-//     }
-  
-//     function revealEmail() {
-//       // Show the email by removing the canvas
-//       canvas.style.display = 'none';
-  
-//       // Enable click functionality for the revealed email
-//       const emailLink = document.querySelector('.gmailLink');
-//       emailLink.style.cursor = 'pointer';
-//       emailLink.addEventListener('click', copyEmailToClipboard);
-//     }
-  
-//     function copyEmailToClipboard() {
-//       const emailText = document.querySelector('.gmailLink').innerText;
-  
-//       // Create a temporary textarea element to copy the text to the clipboard
-//       const textarea = document.createElement('textarea');
-//       textarea.value = emailText;
-//       document.body.appendChild(textarea);
-//       textarea.select();
-//       document.execCommand('copy');
-//       document.body.removeChild(textarea);
-  
-//       // Add visual feedback or notification to indicate the email has been copied
-//       alert('Email copied to clipboard!');
-//     }
-  
-//     function stopDrawing() {
-//       isDrawing = false;
-//     }
-//   }
-
-const gmailOpner = document.querySelector('.green');
-const contactOpener = document.querySelector('.contact');
-const canvas = document.getElementById('canvasScratcher');
-const ctx = canvas.getContext('2d');
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
 let revealThreshold = 0.7; // Percentage of scratched area to trigger reveal
 
-gmailOpner.addEventListener('click', () => {
-  console.log('hi');
-  if (!gmailOpner.classList.contains('clicked')) {
-    gmailOpner.classList.add('clicked');
-    contactOpener.classList.remove('hidden');
-    canvas.style.display = 'block';
-    initCanvas();
-  } else {
-    gmailOpner.classList.remove('clicked');
-    contactOpener.classList.add('hidden');
-    canvas.style.display = 'none';
-  }
-});
+// Add an event listener to the document for a click event
+// Add an event listener to the document for a click event
+document.addEventListener('click', (event) => {
+    if (!contactOpener.contains(event.target) && event.target !== gmailOpner) {
+      contactOpener.classList.add('hidden');
+      canvas.style.display = 'none';
+    }
+  });
+  
+  gmailOpner.addEventListener('click', (event) => {
+    event.stopPropagation();
+  
+    if (contactOpener.classList.contains('hidden')) {
+      contactOpener.classList.remove('hidden');
+      canvas.style.display = 'block';
+      initCanvas();
+    } else {
+      contactOpener.classList.add('hidden');
+      canvas.style.display = 'none';
+    }
+  });
+  
+  
 
 function initCanvas() {
   // Set the canvas size
